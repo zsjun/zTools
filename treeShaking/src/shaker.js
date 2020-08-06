d76afd3dce77287a6f9ad06df07418fa14a8f0e1
zsj <zoushijun@threatbook.cn>
2020-08-06 21:17:19 +0800

    change treeshaking

--- treeShaking/src/shaker.js
+++ treeShaking/src/shaker.js
@@ -9,6 +9,7 @@ const buffer = fs.readFileSync(args).toString();
 // 转成ast
 const body = acorn.parse(buffer).body;
 const jsEmitter = new JSEmitter();
+
 // 记录声明的变量和函数
 let decls = new Map();
 // 记录被调用的变量和函数
@@ -19,13 +20,25 @@ let code = [];
 
 body.forEach(function (node) {
   if (node.type == "FunctionDeclaration") {
-    const code = jsEmitter.run([node]);
-    decls.set(jsEmitter.visitNode(node.id), code);
+    const codeFunc = jsEmitter.run([node]);
+    // 记录声明的函数
+    decls.set(jsEmitter.visitNode(node.id), codeFunc);
     return;
   }
+
+  if (node.type == "VariableDeclaration") {
+    const kind = node.kind;
+    // 记录变量
+    for (const decl of node.declarations) {
+      decls.set(jsEmitter.visitNode(decl.id), jsEmitter.visitVariableDeclarator(decl, kind));
+    }
+    return;
+  }
+
   if (node.type == "ExpressionStatement") {
     if (node.expression.type == "CallExpression") {
       const callNode = node.expression;
+      // 记录被调用的函数和变量
       calledDecls.push(jsEmitter.visitIdentifier(callNode.callee));
       const args = callNode.arguments;
       for (const arg of args) {
@@ -35,16 +48,11 @@ body.forEach(function (node) {
       }
     }
   }
-  if (node.type == "VariableDeclaration") {
-    const kind = node.kind;
-    for (const decl of node.declarations) {
-      decls.set(jsEmitter.visitNode(decl.id), jsEmitter.visitVariableDeclarator(decl, kind));
-    }
-    return;
-  }
+
   if (node.type == "Identifier") {
     calledDecls.push(node.name);
   }
+  // 如果是执行表达式，执行获取代码
   code.push(jsEmitter.run([node]));
 });
 code = calledDecls
