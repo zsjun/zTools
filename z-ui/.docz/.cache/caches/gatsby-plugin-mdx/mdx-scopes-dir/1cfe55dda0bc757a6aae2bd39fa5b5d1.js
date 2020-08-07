c62efdf39a312c46a3840f8c6ecd186dd019d8ae
zsj <zoushijun@threatbook.cn>
2020-08-07 19:44:51 +0800

    feat: delete

--- z-ui/.docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/1cfe55dda0bc757a6aae2bd39fa5b5d1.js
+++
@@ -1,8 +0,0 @@
-import { HappyBox } from "../../../../../doc-comps";
-import BasicDemoCode from "!raw-loader!../../../../../components/alert/demo/1-demo-basic.tsx";
-import * as React from 'react';
-export default {
-  HappyBox,
-  BasicDemoCode,
-  React
-};
